47901a601bcf2d4dea0c9e20f0f128fe961fa045
sowakarol <karosowa@student.agh.edu.pl>
2018-12-04 00:29:44 +0100

    Back button + action bar title

--- app/src/main/java/pl/edu/agh/activities_fragments/ItemDetailActivity.java
+++ app/src/main/java/pl/edu/agh/activities_fragments/ItemDetailActivity.java
@@ -1,9 +1,9 @@
 package pl.edu.agh.activities_fragments;
 
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -13,8 +13,17 @@ public class ItemDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+
         // Fetch the item to display from bundle
         Item item = (Item) getIntent().getSerializableExtra("item");
+
+
+        // if null pointer then activity has a different theme
+        // and getActionBar() may be good
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(item.getTitle());
+        
         if (savedInstanceState == null) {
             // Insert detail fragment based on the item passed
             fragmentItemDetail = ItemDetailFragment.newInstance(item); // <-------
@@ -22,5 +31,12 @@ public class ItemDetailActivity extends AppCompatActivity {
             ft.replace(R.id.flDetailContainer, fragmentItemDetail);
             ft.commit();
         }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
