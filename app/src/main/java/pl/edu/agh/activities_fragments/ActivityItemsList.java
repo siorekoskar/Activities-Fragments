f2413e0cee297af2e48524cfe9c469a280b84910
Siorek, Oskar <oskar.siorek@sabre.com>
2018-12-04 00:44:49 +0100

    added landscape

--- app/src/main/java/pl/edu/agh/activities_fragments/ActivityItemsList.java
+++ app/src/main/java/pl/edu/agh/activities_fragments/ActivityItemsList.java
@@ -1,24 +1,45 @@
 package pl.edu.agh.activities_fragments;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class ActivityItemsList extends AppCompatActivity implements ItemsListFragment.OnListItemSelectedListener {
+
+    // Flag determines if this is a one or two pane layout
+    private boolean isTwoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_list);
+        // Call this to determine which layout we are in (tablet or phone)
+        determinePaneLayout();
+    }
+
+    private void determinePaneLayout() {
+        FrameLayout fragmentItemDetail = (FrameLayout) findViewById(R.id.flDetailContainer);
+        // If there is a second pane for details
+        if (fragmentItemDetail != null) {
+            isTwoPane = true;
+        }
     }
 
     @Override
     public void onItemSelected(Item item) {
-        // For phone, launch detail activity using intent
-        Intent i = new Intent(this, ItemDetailActivity.class);
-        // Embed the serialized item
-        i.putExtra("item", item);
-        // Start the activity
-        startActivity(i);
+        if (isTwoPane) { // single activity with list and detail
+            // Replace framelayout with new detail fragment
+            ItemDetailFragment fragmentItem = ItemDetailFragment.newInstance(item);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flDetailContainer, fragmentItem);
+            ft.commit();
+        } else { // go to separate activity
+            // launch detail activity using intent
+            Intent i = new Intent(this, ItemDetailActivity.class);
+            i.putExtra("item", item);
+            startActivity(i);
+        }
     }
 }
