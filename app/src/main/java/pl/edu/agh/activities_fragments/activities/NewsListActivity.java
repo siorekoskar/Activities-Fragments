b00c0089e907c308bfde77dbf601bf9974872643
sowakarol <karosowa@student.agh.edu.pl>
2018-12-05 23:57:05 +0100

    Task 1

--- app/src/main/java/pl/edu/agh/activities_fragments/activities/NewsListActivity.java
+++ app/src/main/java/pl/edu/agh/activities_fragments/activities/NewsListActivity.java
@@ -1,37 +1,39 @@
 package pl.edu.agh.activities_fragments.activities;
 
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import pl.edu.agh.activities_fragments.R;
 import pl.edu.agh.activities_fragments.domain.News;
-import pl.edu.agh.activities_fragments.fragments.NewsDetailFragment;
-import pl.edu.agh.activities_fragments.fragments.NewsListFragment;
 
-public class NewsListActivity extends AppCompatActivity implements NewsListFragment.OnListItemSelectedListener {
+public class NewsListActivity extends AppCompatActivity {
+    private ArrayAdapter<News> adapterItems;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_list);
+
+        ArrayList<News> news = News.getItems();
+        adapterItems = new ArrayAdapter<News>(this,
+                android.R.layout.simple_list_item_activated_1, news);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(adapterItems);
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View item,
+                                    int position, long rowId) {
+                System.out.println(position);
+            }
+        });
     }
 
-    @Override
-    public void onItemSelected(News news) {
-        int orientation = getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            NewsDetailFragment fragmentItem = NewsDetailFragment.newInstance(news);
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.flDetailContainer, fragmentItem);
-            ft.addToBackStack(null);
-            ft.commit();
-        } else {
-            Intent i = new Intent(this, NewsDetailActivity.class);
-            i.putExtra("news", news);
-            startActivity(i);
-        }
-    }
 }
